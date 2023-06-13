65c77d6baa24a5843b67389dc0abbb45a368fc33
lumanya <shaibulumanya@gmail.com>
2023-06-13 15:19:11 +0300

    add HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,11 +12,12 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_db(exception):
