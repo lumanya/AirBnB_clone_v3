33bd5c9c750c2f58a342738e56dc3d7983c49525
lumanya <shaibulumanya@gmail.com>
2023-06-13 12:16:30 +0300

    add some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Index """
-
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -9,3 +9,17 @@ from flask import jsonify
 def status():
     """ Returns status """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ Returns number of objects by type stats """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
