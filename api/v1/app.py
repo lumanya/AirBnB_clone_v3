50986af6287aea34c94f23cad08b68f5f68d6118
lumanya <shaibulumanya@gmail.com>
2023-06-13 15:31:13 +0300

    add place amenity

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,7 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
 from flask_cors import CORS
 
 app = Flask(__name__)
