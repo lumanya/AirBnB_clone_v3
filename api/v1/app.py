af4ac93ccc1c0a61030ad72f584d1faced395c24
lumanya <shaibulumanya@gmail.com>
2023-06-13 12:10:34 +0300

    fix pep8 validation

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ Flask api to handle objects """
 
 from flask import Flask
-from models import storage 
+from models import storage
 from api.v1.views import app_views
 from os import environ
 from api.v1.views.index import *
@@ -11,10 +11,12 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """ Closes the storage """
     storage.close()
+
 
 if __name__ == "__main__":
     """ Main Function """
@@ -25,5 +27,3 @@ if __name__ == "__main__":
     if not port:
         port = '5000'
     app.run(host=host, port=port, threaded=True)
-
-
